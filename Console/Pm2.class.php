d77d2640de0dd29cf5de84be1e1966e9557ad5cc
Andrew Nagy <andrew.nagy@the159.com>
2017-04-11 20:40:28 -0400

    finalize work

--- Console/Pm2.class.php
+++ Console/Pm2.class.php
@@ -6,10 +6,10 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-//Ask stuff
-use Symfony\Component\Console\Question\ChoiceQuestion;
 //la mesa
 use Symfony\Component\Console\Helper\Table;
+
+use Symfony\Component\Process\Process;
 
 use Symfony\Component\Console\Command\HelpCommand;
 
@@ -18,27 +18,30 @@ class Pm2 extends Command {
 		$this->setName('pm2')
 		->setDescription(_('Manage long running processes'))
 		->setDefinition(array(
-			new InputOption('list', null, InputOption::VALUE_NONE, _('list processes')),
-			new InputOption('log', null, InputOption::VALUE_REQUIRED, _('stream logs')),
-			new InputOption('lines', 15, InputOption::VALUE_REQUIRED, _('stream logs'))
+			new InputOption('list', null, InputOption::VALUE_NONE, _('List Process')),
+			new InputOption('stop', null, InputOption::VALUE_REQUIRED, _('Stop Process')),
+			new InputOption('restart', null, InputOption::VALUE_REQUIRED, _('Restart Process')),
+			new InputOption('delete', null, InputOption::VALUE_REQUIRED, _('Delete Process')),
+			new InputOption('update', null, InputOption::VALUE_NONE, _('Save processes, kill PM2 and restore processes')),
+			new InputOption('log', null, InputOption::VALUE_REQUIRED, _('Stream Logs from Process')),
+			new InputOption('lines', null, InputOption::VALUE_REQUIRED, _('How many lines to stream'))
 		));
 	}
 	protected function execute(InputInterface $input, OutputInterface $output){
 		if($input->getOption('list')){
 			$data = \FreePBX::Pm2()->listProcesses();
 			$table = new Table($output);
-			$table->setHeaders(array(_('App name'),'PID',_('Status'),_('Restart'),_("Uptime"), _("CPU"),_("Mem")));
+			$table->setHeaders(array(_('Process Name'),'PID',_('Status'),_('Restarts'),_("Uptime"), _("CPU"),_("Mem")));
 			$rows = array();
 			foreach($data as $process) {
-				$time = ($process['pm2_env']['status'] == 'online') ? $this->get_date_diff(time(),(int)round($process['pm2_env']['created_at']/1000)) : 0;
 				$rows[] = array(
 					$process['name'],
 					$process['pid'],
 					$process['pm2_env']['status'],
 					$process['pm2_env']['restart_time'],
-					$time,
+					$process['pm2_env']['created_at_human_diff'],
 					$process['monit']['cpu'].'%',
-					$this->human_filesize($process['monit']['memory']),
+					$process['monit']['human_memory'],
 				);
 			}
 			$table->setRows($rows);
@@ -47,11 +50,56 @@ class Pm2 extends Command {
 		}
 		if($input->getOption('log')){
 			$app = $input->getOption('log');
-			\FreePBX::Pm2()->streamLog($app,$input->getOption('lines'));
+			$lines = 10;
+			if ($input->hasParameterOption('--lines')) {
+				$lines = $input->getOption('lines');
+			}
+			$status = \FreePBX::Pm2()->getStatus($app);
+			$logs = array(
+				$status['pm2_env']['pm_err_log_path'],
+				$status['pm2_env']['pm_out_log_path']
+			);
+			$files = implode(' ', $logs);
+			//passthru('tail -f ' . $files);
+			$process = new Process('tail --lines='.$lines.' -f ' . $files);
+			//Timeout for the above process. Not sure if there is a no limit but 42 Years seems long enough.
+			$process->setTimeout(1325390892);
+			$process->run(function ($type, $buffer) {
+				if (Process::ERR === $type) {
+					echo 'ERR > '.$buffer;
+				} else {
+					echo 'OUT > '.$buffer;
+				}
+			});
+			return;
+		}
+		if($input->getOption('restart')){
+			$app = $input->getOption('restart');
+			\FreePBX::Pm2()->restart($app);
+			$output->writeln("Process Restarted");
+			return;
+		}
+		if($input->getOption('stop')){
+			$app = $input->getOption('stop');
+			\FreePBX::Pm2()->stop($app);
+			$output->writeln("Process Stopped");
+			return;
+		}
+		if($input->getOption('update')){
+			$app = $input->getOption('update');
+			\FreePBX::Pm2()->update();
+			$output->writeln("Update PM2 Process");
+			return;
+		}
+		if($input->getOption('delete')){
+			$app = $input->getOption('delete');
+			\FreePBX::Pm2()->delete($app);
+			$output->writeln("Process Stopped and Deleted");
 			return;
 		}
 		$this->outputHelp($input,$output);
 	}
+
 	/**
 	 * @param InputInterface $input
 	 * @param OutputInterface $output
@@ -63,81 +111,4 @@ class Pm2 extends Command {
 		$help->setCommand($this);
 		return $help->run($input, $output);
 	}
-
-	/**
-	 * Get human readable time difference between 2 dates
-	 *
-	 * Return difference between 2 dates in year, month, hour, minute or second
-	 * The $precision caps the number of time units used: for instance if
-	 * $time1 - $time2 = 3 days, 4 hours, 12 minutes, 5 seconds
-	 * - with precision = 1 : 3 days
-	 * - with precision = 2 : 3 days, 4 hours
-	 * - with precision = 3 : 3 days, 4 hours, 12 minutes
-	 *
-	 * From: http://www.if-not-true-then-false.com/2010/php-calculate-real-differences-between-two-dates-or-timestamps/
-	 *
-	 * @param mixed $time1 a time (string or timestamp)
-	 * @param mixed $time2 a time (string or timestamp)
-	 * @param integer $precision Optional precision
-	 * @return string time difference
-	 */
-	private function get_date_diff( $time1, $time2, $precision = 2 ) {
-		// If not numeric then convert timestamps
-		if( !is_int( $time1 ) ) {
-			$time1 = strtotime( $time1 );
-		}
-		if( !is_int( $time2 ) ) {
-			$time2 = strtotime( $time2 );
-		}
-		// If time1 > time2 then swap the 2 values
-		if( $time1 > $time2 ) {
-			list( $time1, $time2 ) = array( $time2, $time1 );
-		}
-		// Set up intervals and diffs arrays
-		$intervals = array( 'year', 'month', 'day', 'hour', 'minute', 'second' );
-		$diffs = array();
-		foreach( $intervals as $interval ) {
-			// Create temp time from time1 and interval
-			$ttime = strtotime( '+1 ' . $interval, $time1 );
-			// Set initial values
-			$add = 1;
-			$looped = 0;
-			// Loop until temp time is smaller than time2
-			while ( $time2 >= $ttime ) {
-				// Create new temp time from time1 and interval
-				$add++;
-				$ttime = strtotime( "+" . $add . " " . $interval, $time1 );
-				$looped++;
-			}
-			$time1 = strtotime( "+" . $looped . " " . $interval, $time1 );
-			$diffs[ $interval ] = $looped;
-		}
-		$count = 0;
-		$times = array();
-		foreach( $diffs as $interval => $value ) {
-			// Break if we have needed precission
-			if( $count >= $precision ) {
-				break;
-			}
-			// Add value and interval if value is bigger than 0
-			if( $value > 0 ) {
-				if( $value != 1 ){
-					$interval .= "s";
-				}
-				// Add value and interval to times array
-				$times[] = $value . " " . $interval;
-				$count++;
-			}
-		}
-		// Return string with times
-		return implode( ", ", $times );
-	}
-
-	function human_filesize($bytes, $dec = 2) {
-		$size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-		$factor = floor((strlen($bytes) - 1) / 3);
-
-		return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
-	}
-
 }
--- Pm2.class.php
+++ Pm2.class.php
@@ -11,14 +11,14 @@ use Symfony\Component\Console\Helper\ProgressBar;
 class Pm2 extends \FreePBX_Helpers implements \BMO {
 	private $nodever = "0.12.18";
 	private $npmver = "2.15.11";
-	private $pm2 = "/tmp";
+	private $pm2Home = "/tmp";
 	private $nodeloc = "/tmp";
 
 	public function __construct($freepbx = null) {
 		$this->astman = $freepbx->astman;
 		$this->db = $freepbx->Database;
 		$this->freepbx = $freepbx;
-		$this->pm2 = $this->getHomeDir() . "/.pm2";
+		$this->pm2Home = $this->getHomeDir() . "/.pm2";
 		$this->nodeloc = __DIR__."/node";
 	}
 
@@ -107,7 +107,8 @@ class Pm2 extends \FreePBX_Helpers implements \BMO {
 			return false;
 		}
 
-		//PM2_HOME
+		$this->runPM2Command("ping");
+		$this->runPM2Command("update");
 	}
 
 	public function uninstall() {
@@ -125,7 +126,14 @@ class Pm2 extends \FreePBX_Helpers implements \BMO {
 	public function doConfigPageInit($page) {
 	}
 
+	/**
+	 * Get status of a process
+	 * @method getStatus
+	 * @param  string    $name The process name
+	 * @return mixed          Return array of data if known or false if unknown
+	 */
 	public function getStatus($name) {
+		$name = $this->cleanAppName($name);
 		$processes = $this->listProcesses();
 		foreach($processes as $process) {
 			if($process['name'] == $name) {
@@ -135,46 +143,126 @@ class Pm2 extends \FreePBX_Helpers implements \BMO {
 		return false;
 	}
 
+	/**
+	 * Start an application in the background
+	 * @method start
+	 * @param  string $name    The name of the application
+	 * @param  string $process The process to run
+	 * @return mixed           Output of getStatus
+	 */
 	public function start($name, $process) {
+		$name = $this->cleanAppName($name);
+		$pout = $this->getStatus($name);
+		if(!empty($pout) && $pout['pm2_env']['status'] == 'online') {
+			throw new \Exception("There is already a process by that name running!");
+		}
 		$astlogdir = $this->freepbx->Config->get("ASTLOGDIR");
-		$name = preg_replace("/\W\D/", "", $name);
-		$this->runPM2Command("start ".$process." --name ".$name." -e ".$astlogdir."/".$name."_err.log -o ".$astlogdir."/".$name."_out.log --merge-logs");
+		$cwd = dirname($process);
+		$this->runPM2Command("start ".$process." --name ".escapeshellarg($name)." -e ".escapeshellarg($astlogdir."/".$name."_err.log")." -o ".escapeshellarg($astlogdir."/".$name."_out.log")." --merge-logs --log-date-format 'YYYY-MM-DD HH:mm Z'", $cwd);
+		return $this->getStatus($name);
 	}
 
+	/**
+	 * Stop an application from running in the background
+	 * @method stop
+	 * @param  string $name The application name
+	 */
 	public function stop($name) {
+		$name = $this->cleanAppName($name);
+		$out = $this->getStatus($name);
+		if(empty($out)) {
+			throw new \Exception("There is no process by that name");
+		}
 		$this->runPM2Command("stop ".$name);
+	}
+
+	/**
+	 * Restart process
+	 * @method restart
+	 * @param  string  $name The application name
+	 */
+	public function restart($name) {
+		$name = $this->cleanAppName($name);
+		$out = $this->getStatus($name);
+		if(empty($out)) {
+			throw new \Exception("There is no process by that name");
+		}
+		$this->runPM2Command("restart ".$name);
+	}
+
+	/**
+	 * Delete process
+	 * @method delete
+	 * @param  string  $name The application name
+	 */
+	public function delete($name) {
+		$name = $this->cleanAppName($name);
+		$out = $this->getStatus($name);
+		if(empty($out)) {
+			throw new \Exception("There is no process by that name");
+		}
+		$this->runPM2Command("delete ".$name);
+	}
+
+	/**
+	 * Update the underlying PM2 process
+	 * @method update
+	 */
+	public function update() {
+		$this->runPM2Command("update",true);
+	}
+
+	/**
+	 * List Processes that PM2 is maintaining
+	 * @method listProcesses
+	 * @return array        Array of processes
+	 */
+	public function listProcesses() {
+		$output = $this->runPM2Command("jlist");
+		$processes = json_decode($output,true);
+		$final = array();
+		foreach($processes as $process) {
+			$process['pm2_env']['created_at_human_diff'] = ($process['pm2_env']['status'] == 'online') ? $this->get_date_diff(time(),(int)round($process['pm2_env']['created_at']/1000)) : 0;
+			$process['monit']['human_memory'] = $this->human_filesize($process['monit']['memory']);
+			$final[] = $process;
+		}
+		return $final;
+	}
+
+	/**
+	 * Run a command against PM2
+	 * @method runPM2Command
+	 * @param  string        $cmd    The command to run
+	 * @param  boolean       $stream Whether to stream the output or return it
+	 */
+	private function runPM2Command($cmd,$cwd='',$stream=false) {
+		$command = $this->generateRunAsAsteriskCommand($this->nodeloc."/node_modules/pm2/bin/pm2 ".$cmd,$cwd);
+		$process = new Process($command);
+		if(!$stream) {
+			$process->mustRun();
+			return $process->getOutput();
+		} else {
+			$process->setTty(true);
+			$process->setTimeout(60);
+			$process->run(function ($type, $buffer) {
+				if (Process::ERR === $type) {
+					echo $buffer;
+				} else {
+					echo $buffer;
+				}
+			});
+		}
 	}
 
 	public function chownFreepbx() {
 		$files = array();
+		$files[] = array('type' => 'execdir',
+			'path' => __DIR__.'/node/node_modules/pm2/bin',
+			'perms' => 0755);
+		$files[] = array('type' => 'rdir',
+			'path' => $this->pm2Home,
+			'perms' => 0775);
 		return $files;
-	}
-
-	public function streamLog($app,$lines=15) {
-		$command = $this->generateRunAsAsteriskCommand($this->nodeloc."/node_modules/pm2/bin/pm2 log ".$app." --lines ".$lines);
-		$process = new Process($command);
-		$process->setTty(true);
-		$process->setTimeout(1325390892);
-		$process->run(function ($type, $buffer) {
-			if (Process::ERR === $type) {
-				echo 'ERR > '.$buffer;
-			} else {
-				echo 'OUT > '.$buffer;
-			}
-		});
-	}
-
-	public function listProcesses() {
-		$output = $this->runPM2Command("jlist");
-		$data = json_decode($output,true);
-		return $data;
-	}
-
-	private function runPM2Command($cmd) {
-		$command = $this->generateRunAsAsteriskCommand($this->nodeloc."/node_modules/pm2/bin/pm2 ".$cmd);
-		$process = new Process($command);
-		$process->mustRun();
-		return $process->getOutput();
 	}
 
 	/**
@@ -204,7 +292,7 @@ class Pm2 extends \FreePBX_Helpers implements \BMO {
 	 * @param  string                       $command The command to run
 	 * @return string                                The finalized command
 	 */
-	private function generateRunAsAsteriskCommand($command) {
+	private function generateRunAsAsteriskCommand($command,$cwd='') {
 		$webuser = $this->freepbx->Config->get('AMPASTERISKWEBUSER');
 		$webgroup = $this->freepbx->Config->get('AMPASTERISKWEBGROUP');
 		$webroot = $this->freepbx->Config->get("AMPWEBROOT");
@@ -212,12 +300,12 @@ class Pm2 extends \FreePBX_Helpers implements \BMO {
 		$astlogdir = $this->freepbx->Config->get("ASTLOGDIR");
 
 		$cmds = array(
-			'cd '.$this->nodeloc,
-			'mkdir -p '.$this->pm2,
+			'cd '.(!empty($cwd) ? $cwd : $this->nodeloc),
+			'mkdir -p '.$this->pm2Home,
 			'mkdir -p '.$this->nodeloc.'/logs',
 			'export HOME="'.$this->getHomeDir().'"',
 			'echo "prefix = ~/.node" > ~/.npmrc',
-			'export PM2_HOME="'.$this->pm2.'"',
+			'export PM2_HOME="'.$this->pm2Home.'"',
 			'export ASTLOGDIR="'.$astlogdir.'"',
 			'export PATH="$HOME/.node/bin:$PATH"',
 			'export NODE_PATH="$HOME/.node/lib/node_modules:$NODE_PATH"',
@@ -230,5 +318,98 @@ class Pm2 extends \FreePBX_Helpers implements \BMO {
 			$final = "runuser -l ".escapeshellarg($webuser)." -c ".escapeshellarg($final);
 		}
 		return $final;
+	}
+
+	/**
+	 * Turn all spaces into underscores and remove all utf8
+	 * from filenames
+	 * @param  string $name The filename
+	 * @return string       The cleaned filename
+	 */
+	private function cleanAppName($name) {
+		$name = pathinfo($name,PATHINFO_FILENAME);
+		$name = str_replace(" ","-",$name);
+		if(function_exists('iconv')) {
+			$name = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $name);
+		}
+		$name = preg_replace('/\s+|\'+|\\\+|\$+|`+|\"+|<+|>+|\?+|\*+|,+|\.+|&+|;+|\/+/','-',$name);
+		$name = preg_replace('/[\x00-\x1F\x80-\xFF]/u', '', $name);
+		return $name;
+	}
+
+	/**
+	 * Get human readable time difference between 2 dates
+	 *
+	 * Return difference between 2 dates in year, month, hour, minute or second
+	 * The $precision caps the number of time units used: for instance if
+	 * $time1 - $time2 = 3 days, 4 hours, 12 minutes, 5 seconds
+	 * - with precision = 1 : 3 days
+	 * - with precision = 2 : 3 days, 4 hours
+	 * - with precision = 3 : 3 days, 4 hours, 12 minutes
+	 *
+	 * From: http://www.if-not-true-then-false.com/2010/php-calculate-real-differences-between-two-dates-or-timestamps/
+	 *
+	 * @param mixed $time1 a time (string or timestamp)
+	 * @param mixed $time2 a time (string or timestamp)
+	 * @param integer $precision Optional precision
+	 * @return string time difference
+	 */
+	private function get_date_diff( $time1, $time2, $precision = 2 ) {
+		// If not numeric then convert timestamps
+		if( !is_int( $time1 ) ) {
+			$time1 = strtotime( $time1 );
+		}
+		if( !is_int( $time2 ) ) {
+			$time2 = strtotime( $time2 );
+		}
+		// If time1 > time2 then swap the 2 values
+		if( $time1 > $time2 ) {
+			list( $time1, $time2 ) = array( $time2, $time1 );
+		}
+		// Set up intervals and diffs arrays
+		$intervals = array( 'year', 'month', 'day', 'hour', 'minute', 'second' );
+		$diffs = array();
+		foreach( $intervals as $interval ) {
+			// Create temp time from time1 and interval
+			$ttime = strtotime( '+1 ' . $interval, $time1 );
+			// Set initial values
+			$add = 1;
+			$looped = 0;
+			// Loop until temp time is smaller than time2
+			while ( $time2 >= $ttime ) {
+				// Create new temp time from time1 and interval
+				$add++;
+				$ttime = strtotime( "+" . $add . " " . $interval, $time1 );
+				$looped++;
+			}
+			$time1 = strtotime( "+" . $looped . " " . $interval, $time1 );
+			$diffs[ $interval ] = $looped;
+		}
+		$count = 0;
+		$times = array();
+		foreach( $diffs as $interval => $value ) {
+			// Break if we have needed precission
+			if( $count >= $precision ) {
+				break;
+			}
+			// Add value and interval if value is bigger than 0
+			if( $value > 0 ) {
+				if( $value != 1 ){
+					$interval .= "s";
+				}
+				// Add value and interval to times array
+				$times[] = $value . " " . $interval;
+				$count++;
+			}
+		}
+		// Return string with times
+		return implode( ", ", $times );
+	}
+
+	function human_filesize($bytes, $dec = 2) {
+		$size   = array('B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+		$factor = floor((strlen($bytes) - 1) / 3);
+
+		return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
 	}
 }
