bd9d4f7f5a590d2dcc8e30bb7f7dd0ab68eec30f
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 19:15:32 -0500

    [Module Tag script: pm2 15.0.3.2] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,36 +2,34 @@
 namespace FreePBX\modules\Pm2;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-    $configs = $this->getConfigs();
-    $advancedSettings = [
-      'PM2USEPROXY' => false,
-      'NODEJSBINDADDRESS' => 'http://mirror.freepbx.org:6767/',
-      'PM2USECACHE' => true,
-      'PM2SHELL' => '/bin/bash',
-      'PM2DISABLELOG' => false
-    ];
-    foreach($configs as $setting => $value){
-      if(!isset($advancedSettings[$setting])){
-        continue;
-      }
-      $this->FreePBX->Config->set($setting,$value);
-    }
-  }
-  public function processLegacy($pdodbconn, $data, $tablelist, $unknowntables, $tmpdir){
-    $settings = [
-      'PM2USEPROXY' => false,
-      'NODEJSBINDADDRESS' => 'http://mirror.freepbx.org:6767/',
-      'PM2USECACHE' => true,
-      'PM2SHELL' => '/bin/bash',
-      'PM2DISABLELOG' => false
-    ];
-    $configs = $this->getAMPConf($pdodbconn);
-    foreach ($configs as $setting => $value) {
-      if (!isset($settings[$setting])) {
-        continue;
-      }
-      $this->FreePBX->Config->set($setting, $settings[$setting]);
-    }
-  }
+	public function runRestore($jobid){
+		$configs = $this->getConfigs();
+		$advancedSettings = [
+			'PM2USEPROXY' => false,
+			'PM2PROXY' => 'http://mirror.freepbx.org:6767/',
+			'PM2USECACHE' => true,
+			'PM2SHELL' => '/bin/bash',
+			'PM2DISABLELOG' => false
+		];
+		foreach($configs as $setting => $value){
+			if(!isset($advancedSettings[$setting])){
+				continue;
+			}
+			$this->FreePBX->Config->set($setting,$value);
+		}
+	}
+	public function processLegacy($pdodbconn, $data, $tablelist, $unknowntables){
+		$advanced = [
+			'PM2USEPROXY',
+			'PM2PROXY',
+			'PM2USECACHE',
+			'PM2SHELL',
+			'PM2DISABLELOG'
+		];
+		foreach ($advanced as $key) {
+			if(isset($data['settings'][$key])){
+				$this->FreePBX->Config->update($key, $data['settings'][$key]);
+			}
+		}
+	}
 }
