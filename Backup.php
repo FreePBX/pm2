bbd00526f10fd1d3253671804de59106f47b69fc
Andrew Nagy <andrew.nagy@the159.com>
2019-03-06 18:33:47 -0500

    [Module Tag script: pm2 15.0.3.6]

--- Backup.php
+++ Backup.php
@@ -2,19 +2,9 @@
 namespace FreePBX\modules\Pm2;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
-  public function runBackup($id,$transaction){
-    $configs = [];
-    $advancedSettings = [
-      'PM2USEPROXY' => false,
-      'NODEJSBINDADDRESS' => 'http://mirror.freepbx.org:6767/',
-      'PM2USECACHE' => true,
-      'PM2SHELL' => '/bin/bash',
-      'PM2DISABLELOG' => false
-    ];
-    foreach($advancedSettings as $setting => $default){
-      $ret = $this->FreePBX->Config->get($setting);
-      $configs[$setting] = ($ret)?$ret:$default;
-    }
-    $this->addConfigs($configs);
-  }
+	public function runBackup($id,$transaction){
+		$this->addConfigs([
+			'settings' => $this->dumpAdvancedSettings()
+		]);
+	}
 }
--- Restore.php
+++ Restore.php
@@ -2,34 +2,11 @@
 namespace FreePBX\modules\Pm2;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
-		$advancedSettings = [
-			'PM2USEPROXY' => false,
-			'PM2PROXY' => 'http://mirror.freepbx.org:6767/',
-			'PM2USECACHE' => true,
-			'PM2SHELL' => '/bin/bash',
-			'PM2DISABLELOG' => false
-		];
-		foreach($configs as $setting => $value){
-			if(!isset($advancedSettings[$setting])){
-				continue;
-			}
-			$this->FreePBX->Config->set($setting,$value);
-		}
+		$this->importAdvancedSettings($configs['settings']);
 	}
-	public function processLegacy($pdodbconn, $data, $tablelist, $unknowntables){
-		$advanced = [
-			'PM2USEPROXY',
-			'PM2PROXY',
-			'PM2USECACHE',
-			'PM2SHELL',
-			'PM2DISABLELOG'
-		];
-		foreach ($advanced as $key) {
-			if(isset($data['settings'][$key])){
-				$this->FreePBX->Config->update($key, $data['settings'][$key]);
-			}
-		}
+	public function processLegacy($pdo, $data, $tablelist, $unknowntables){
+		$this->restoreLegacyAdvancedSettings($pdo);
 	}
 }
